c4d07e5169d4b878e605aeb0655d5283084fab94
Артём Ильин <artemilin347@gmail.com>
2023-11-24 20:38:04 +0300

    бля не помню что поменял :)

--- LW2.cpp
+++ LW2.cpp
@@ -1,7 +1,7 @@
 /****************************************************************
 *                     КАФЕДРА № 304 1 КУРС                      *
 *---------------------------------------------------------------*
-* Project Type  : Console Application				*
+* Project Type  : Console Application                           *
 * Project Name  : LW2                                           *
 * File Name     : LW2.cpp                                       *
 * Language      : C/C++                                         *
@@ -20,7 +20,9 @@
 
 using namespace std;
 
-void print_table_line(ostream& outp, const string& left_border, const string& line, const string& divider, const string& right_border, int cell_amount = 3, int cell_width = 20);
+const int CELL_WIDTH = 20;
+
+void print_table_line(ostream& outp, const string& left_border, const string& line, const string& divider, const string& right_border, int cell_amount = 3, int cell_width = CELL_WIDTH);
 double f(double x);
 double g(double x);
 
@@ -28,7 +30,7 @@ int main()
 {
     вперед_славяне;
     double a, b; // Границы отрезка
-    int n = 0; // Количество секций (подотрезков)
+    int n = 1; // Количество секций (подотрезков)
     double h; // Длина одной секции
     double x; // Текущий аргумент функции
 
@@ -39,7 +41,6 @@ int main()
 
     if (a > b) {
         cout << "Левая граница должна быть левее правой границы!\n";
-        system("pause");
         return 1;
     }
 
@@ -48,36 +49,42 @@ int main()
 
         if (n < 1) {
             cout << "Количество отрезков должно быть больше 0!\n";
-            system("pause");
             return 1;
         }
+
+        ++n;
     }
 
     cout << '\n';
 
     x = a;
     h = (b-a) / n;
+    cout << "h " << h << '\n';
+    cout << "a " << a << '\n';
+    cout << "b " << b << '\n';
+    cout << "n " << n << '\n';
 
     print_table_line(cout, "╔", "═", "╤", "╗");
 
-    cout << "║" << setw(20) << 'x' << "│" << setw(20) << "f(x)" << "│" << setw(20) << "g(x)" << "║" << '\n';
+    cout << "║" << setw(CELL_WIDTH) << 'x' << "│" << setw(CELL_WIDTH) << "f(x)" << "│" << setw(CELL_WIDTH) << "g(x)" << "║" << '\n';
 
     print_table_line(cout, "╠", "═", "╪", "╣");
 
-    for (int i = 0; i < n+1; ++i) {
+    for (int i = 0; i < n; ++i) {
         if(i % 3 == 0 && i != 0) {
             print_table_line(cout, "╟", "─", "┼", "╢");
         }
         resultF = f(x);
         resultG = g(x);
 
-        cout << "║" << setw(20) << x  << "│" << setw(20) << resultF << "│" << setw(20) << resultG << "║" << '\n';
+        cout << "║" << setw(CELL_WIDTH) << x  << "│" << setw(CELL_WIDTH) << resultF << "│" << setw(CELL_WIDTH) << resultG << "║" << '\n';
         x += h;
     }
 
     print_table_line(cout, "╚", "═", "╧", "╝");
 
-    system("pause");
+    cout << endl;
+
     return 0;
 }
 
