74d5e78657d630ea79bea05ecfb9d6803e9e619d
LightChimera <artemilin347@gmail.com>
2024-05-28 10:52:18 +0300

    Добавить тесты для втрой лабы

--- second-sem/LW2/run_tests.sh
+++ second-sem/LW2/run_tests.sh
@@ -5,7 +5,7 @@ cmake --build build || exit $?
 
 for file in tests/*; do
     echo "RUNNER: $file"
-    ./build/LW2 "$file"
+    ./build/LW2-arrays "$file"
     echo "RUNNER DONE: $?"
     echo
     echo
