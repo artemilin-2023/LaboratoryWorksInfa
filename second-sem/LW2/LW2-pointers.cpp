67f355dafb412a2601f746ab40cf53ee8c7766db
y7o4ka <56449763+4310V343k@users.noreply.github.com>
2024-10-30 20:55:22 +0300

    flack off

--- second-sem/LW2/LW2-pointers.cpp
+++ second-sem/LW2/LW2-pointers.cpp
@@ -12,85 +12,130 @@
  * Comment(s)    : Двумерные массивы                             *
  ****************************************************************/
 
-#define _USE_MATH_DEFINES
-
 #include <iostream>
-#include <filesystem>
 #include <fstream>
 #include <limits>
 
-size_t str_len(const char *text);  // our own ffs
-void swap_char(char &a, char &b);  // same
-void reset_stream(std::ifstream &stream, const std::streampos start_pos);
-int validate_data(std::ifstream &in);
-int mult_neg_elems_above_diag(int* arr, size_t height, size_t width);
-int min_uneven_elems(int* arr, size_t length);
+void handle_file(const char* file_name);
+int validate_and_read_matrix(std::ifstream& in, int* &matrix, int &height, int &width);
+void print_matrix(const int* matrix, int height, int width);
+int mult_neg_elems_above_diag(const int* arr, int height, int width);
+int min_uneven_elems(const int* arr, int height, int width);
 
 int main(int argc, char **argv) {
     if(argc == 1) {
-        std::cout << "Usage: " << argv[0] << " test_file" << '\n';
+        std::cout << "Usage: " << argv[0] << " test_file_A test_file_B" << '\n';
         return 0;
     }
 
-    size_t matrix_amount;
-    std::ifstream input_file(argv[1]);  // Поток чтения из файла.
+    std::cout << "Файл 1: " << argv[1] << '\n';
+    handle_file(argv[1]);
+
+    std::cout << "Файл 2: " << argv[2] << '\n';
+    handle_file(argv[2]);
+}
+
+void handle_file(const char* file_name) {
+    std::ifstream input_file(file_name);
 
     if (!input_file.is_open()) {
         std::cerr << "Ошибка открытия файла. Убедитесь, что по указанному пути существует файл: "
-                  << argv[1] << '\n';
-        return -1;
+                  << file_name << '\n';
+        return;
     }
 
     if (input_file.peek() == EOF) {
-        std::cerr << "Файл пуст.\n";
-        return -2;
+        std::cerr << "Файл " << file_name << " пуст.\n";
+        return;
     }
 
-    if (int ecode = validate_data(input_file); ecode != 0) {
+    int *matrix, height, width;
+
+    if(0 != validate_and_read_matrix(input_file, matrix, height, width)) {
         std::cerr << "В файле должны через пробел или новую строку находиться количество матриц, а затем для каждой высота, ширина, а затем элементы матрицы слева-направо, затем сверху-вниз. Пример:\n";
         std::cerr << "1\n2 3\n1 2 3\n4 5 6\n";
         std::cerr << "Исходный файл:\n";
+
+        input_file.clear();
+        input_file.seekg(std::ios_base::beg);
+
         char c;
         while(input_file.get(c))
             std::cerr << c;
         std::cerr << '\n';
-        return ecode;
+        return;
     }
 
-    input_file >> matrix_amount;
-    for(size_t m_i = 0; m_i < matrix_amount; m_i++) {
-        std::cout << m_i+1 << "-я матрица: \n";
-        size_t height, width;
-        input_file >> height >> width;
-        int *arr = new int[height*width];
-        for(int *arr_cur = arr, *arr_end = arr + height*width; arr_cur < arr_end; arr_cur++) {
-            int n; input_file >> n;
-            *arr_cur = n;
-            std::cout << n << ' ';
-            if((arr_cur - arr) % width == width-1)
-                std::cout << '\n';
-        }
-        int task1 = mult_neg_elems_above_diag(arr, height, width);
-        if(task1 == 0)
-            std::cout << "Негативных элементов выше главной диагонали нет!\n";
-        else
-            std::cout << "Произведение негативных элементов выше главной диагонали: " << task1 << '\n';
+    std::cout << "Исходная матрица:\n";
+    print_matrix(matrix, height, width);
 
-        int task2 = min_uneven_elems(arr, height * width);
-        if(task2 == 0)
-            std::cout << "Нечётных элементов нет!\n";
-        else
-            std::cout << "Минимум среди нечётных элементов: " << task2 << '\n';
-        std::cout << '\n';
+    int task1 = mult_neg_elems_above_diag(matrix, height, width);
+    if(task1 == 0)
+        std::cout << "Негативных элементов выше главной диагонали нет!\n";
+    else
+        std::cout << "Произведение негативных элементов выше главной диагонали: " << task1 << '\n';
+
+    int task2 = min_uneven_elems(matrix, height, width);
+    if(task2 == 0)
+        std::cout << "Нечётных элементов нет!\n";
+    else
+        std::cout << "Минимум среди нечётных элементов: " << task2 << '\n';
+
+    delete[] matrix;
+}
+
+int validate_and_read_matrix(std::ifstream& in, int* &matrix, int &height, int &width) {
+    int n;
+    in >> height >> width;
+    if(in.fail()) { // not numbers? not enough data? bad either way
+        std::cerr << "Не удалось получить размерность матрицы.\n";
+        if(in.eof())
+            std::cerr << "Недостаточно данных в файле.\n";
+        return -5;
+    }
+    std::cout << "Полученная размерность матрицы: " << height << 'x' << width << '\n';
+    if(height < 1 || width < 1) {
+        std::cerr << "Высота и ширина должны быть больше 0!'\n";
+        return -6;
+    }
+
+    matrix = new int[height*width];
+
+    for(int i = 0; i < height*width; i++) {
+        in >> n;
+
+        if(in.fail()) { // not numbers? not enough data? bad either way
+            std::cerr << "Ошибка получения данных данных " << i << "-го элемента матрицы.\n";
+            if(in.eof())
+                std::cerr << "Недостаточно данных в файле.\n";
+            return -5;
+        }
+
+        matrix[i] = n;
+    }
+
+    in >> n;
+    if(!in.eof()) { // there is more data?
+        std::cerr << "В файле содержится больше данных, чем нужно для представления заданных матриц.\n";
+        return -7;
+    }
+    return 0;
+}
+
+void print_matrix(const int* const matrix, int height, int width) {
+    for(const int *arr_cur = matrix, *arr_end = matrix + height*width; arr_cur < arr_end; arr_cur++) {
+        std::cout << *arr_cur << ' ';
+        if((arr_cur - matrix) % width == width-1)
+            std::cout << '\n';
     }
 }
 
-int mult_neg_elems_above_diag(int* arr_start, size_t height, size_t width) {
+int mult_neg_elems_above_diag(const int* const arr_start, int height, int width) {
     bool neg_found = false;
     int mult = 1;
     int shift = 0;
-    int *arr_cur = arr_start;
-    int *arr_end = arr_start + height*width;
+    const int *arr_cur = arr_start;
+    const int *arr_end = arr_start + height*width;
     while(arr_cur < arr_end) {
         if((arr_cur - arr_start) % width == 0) {
             arr_cur += ++shift;
@@ -106,12 +151,12 @@ int mult_neg_elems_above_diag(int* arr_start, size_t height, size_t width) {
     return mult;
 }
 
-int min_uneven_elems(int* arr, size_t length) {
+int min_uneven_elems(const int* arr, int height, int width) {
     bool uneven_found = false;
     int min = std::numeric_limits<int>::max();
-    int* arr_end = arr + length;
+    const int* arr_end = arr + height*width;
     while(arr < arr_end) {
-        if(*arr % 2 == 0) {
+        if(*arr % 2 != 0) {
             uneven_found = true;
             min = *arr < min ? *arr : min;
         }
@@ -120,62 +165,4 @@ int min_uneven_elems(int* arr, size_t length) {
     if(!uneven_found)
         return 0;
     return min;
-}
-
-void reset_stream(std::ifstream &stream, const std::streampos start_pos) {
-    stream.clear();
-    stream.seekg(start_pos);
-}
-
-int validate_data(std::ifstream &in) {
-    int m_amt, N, M, n;
-    in >> m_amt; // number of matrices to process
-    if(in.fail()) { // not numbers? not enough data? bad either way
-        std::cerr << "Не удалось получить количество матриц из файла.\n";
-        if(in.eof())
-            std::cerr << "Недостаточно данных в файле.\n";
-        reset_stream(in, std::ios_base::beg);
-        return -4;
-    }
-
-    std::cout << "Полученное количество матриц: " << m_amt << '\n';
-    if(m_amt < 1) {
-        std::cerr << "Количество матриц должно быть больше нуля!\n";
-        return -5;
-    }
-    for(size_t m_i = 0; m_i < m_amt; m_i++) {
-        in >> N >> M;
-        if(in.fail()) { // not numbers? not enough data? bad either way
-            std::cerr << "Не удалось получить размерность " << m_i << "-й матрицы.\n";
-            if(in.eof())
-                std::cerr << "Недостаточно данных в файле.\n";
-            reset_stream(in, std::ios_base::beg);
-            return -4;
-        }
-        std::cout << "Полученная размерность " << m_i+1 << "-й матрицы: " << N << 'x' << M << '\n';
-        if(N < 1 || M < 1) {
-            std::cerr << "Высота и ширина должны быть больше 0!'\n";
-            return -5;
-        }
-
-        for(size_t i = 0; i < N*M; i++) {
-            in >> n; // just try to get all data for this matrix
-
-            if(in.fail()) { // not numbers? not enough data? bad either way
-                std::cerr << "Ошибка получения данных данных " << i << "-го элемента " << m_i << "-й матрицы.\n";
-                if(in.eof())
-                    std::cerr << "Недостаточно данных в файле.\n";
-                reset_stream(in, std::ios_base::beg);
-                return -4;
-            }
-        }
-    }
-    in >> n;
-    if(!in.eof()) { // there is more data..?
-        std::cerr << "В файле содержится больше данных, чем нужно для представления заданных матриц.\n";
-        reset_stream(in, std::ios_base::beg);
-        return -6;
-    }
-    reset_stream(in, std::ios_base::beg);
-    return 0;
 }
